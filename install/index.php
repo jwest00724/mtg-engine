8300e98641a27240fa0d6b0d3ddd264bdb5f1048
Magictallguy <magictallguy@hotmail.com>
2015-05-08 04:46:46 +0100

    Major bug fixes throughout

--- install/index.php
+++ install/index.php
@@ -1,12 +1,14 @@
 <?php
-include(__DIR__ . '/header.php');
+require_once(__DIR__ . '/header.php');
 ?><div class="header">
 	<h1>MTG Codes v9</h1>
 	<h2>Welcome to MTG Codes v9's very own installer!</h2>
 </div>
 <div class="content">
-	<h2 class="content-subhead">How to use this installer</h2>
-	<p>
+	<h2 class="content-subhead">How to use this installer</h2><?php
+	if(file_exists(DIRNAME(__DIR__) . '/includes/config.php'))
+		echo "<div class='notification notification-info'><i class='fa fa-info-circle'></i><p>It looks like your game may have already been installed.. Please check that <code>/includes/config.php</code> contains the correct information</p></div>";
+	?><p>
 		Over the next few steps, you're going to install MTG Codes v9.<br />
 		Seeing as you're probably reading this from a browser, I'm going to assume that you've already uploaded all files and have read the steps in the README to reach this place - if not, <strong>go read them now!</strong><br /><br />
 		Most of the info you're about to fill in can be easily changed in-game. The database configuration settings, however, require you to edit <code>/includes/config.php</code>.<br /><br />
