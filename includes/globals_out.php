02da4a3ec03b27d5d43e80c20b456de78b8ae26d
Magictallguy <magictallguy@hotmail.com>
2015-05-08 04:46:14 +0100

    Minor bug fix

--- includes/globals_out.php
+++ includes/globals_out.php
@@ -3,11 +3,11 @@ session_start();
 ob_start();
 if(!defined('MTG_ENABLE'))
 	define('MTG_ENABLE', true);
-if(!file_exists(__DIR__ . '/includes/config.php'))
+if(!file_exists(__DIR__ . '/config.php'))
 	exit(header("Location: install"));
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/class/class_mtg_db_mysqli.php');
-if($db->tableExists('game_settings'))
+if(!$db->tableExists('game_settings'))
 	exit(header("Location: install"));
 $set   = array();
 $db->query("SELECT value, name FROM game_settings");
@@ -15,7 +15,11 @@ $db->execute();
 $row = $db->fetch_row();
 foreach($row as $r)
 	$set[$r['name']] = $r['value'];
-error_reporting(0);
+error_reporting(E_ALL);
 require_once(__DIR__ . '/class/class_mtg_functions.php');
 if(!isset($mtg))
 	$mtg = new mtg_functions;
+if(!defined('NO_HEAD')) {
+	require_once(__DIR__ . '/header_out.php');
+	$h = headers::getInstance($set);
+}
