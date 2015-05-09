5116f5a8f63464c9c26519dd006df9a58d56a216
Magictallguy <magictallguy@hotmail.com>
2015-05-09 17:12:25 +0100

    Added jail, hospital, markets

--- includes/menu.php
+++ includes/menu.php
@@ -5,8 +5,11 @@ $links = array(
 	'index' => 'Home',
 	'messages' => 'Messages [msg_count]',
 	'events' => 'Notifications [ev_count]',
+	'hospital' => 'Hospital [hosp_count]',
+	'jail' => 'Jail [jail_count]',
 	'crimes' => 'Crimes',
 	'gym' => 'Gym',
+	'markets' => 'Markets'
 );
 ?><div id='side-menu'>
 	<ul><?php
@@ -20,6 +23,16 @@ $links = array(
 				$db->query("SELECT COUNT(id) FROM users_events WHERE `read` = 0 AND user = ?");
 				$db->execute(array($my['id']));
 				$disp = str_replace('[ev_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
+			}
+			if(preg_match('/\[hosp_count\]/', $disp)) {
+				$db->query("SELECT COUNT(id) FROM users WHERE `hospital` > ?");
+				$db->execute(array(time()));
+				$disp = str_replace('[hosp_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
+			}
+			if(preg_match('/\[jail_count\]/', $disp)) {
+				$db->query("SELECT COUNT(id) FROM users WHERE `jail` > ?");
+				$db->execute(array(time()));
+				$disp = str_replace('[jail_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
 			}
 			printf("<li%s><a href='%s.php'>%s</a></li>", $_SERVER['PHP_SELF'] == '/'.$url.'.php' ? " class='current_page_item'" : '', $url, $disp);
 		}
