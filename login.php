1cb8e94ef4ef2421e724d59987aa55122fc23d98
Magictallguy <magictallguy@hotmail.com>
2015-05-18 16:46:22 +0100

    New style

--- login.php
+++ login.php
@@ -4,19 +4,10 @@ if(isset($_SESSION['msg'])) {
 	$mtg->error($_SESSION['msg'], false);
 	unset($_SESSION['msg']);
 }
-?><form action='auth.php' method='post' class='pure-form pure-form-aligned'>
-	<div style='float:auto;'>
-		<fieldset>
-			<legend>Login</legend>
-			<div class='pure-control-group'>
-				<label for='username'>Username</label>
-				<input type='text' name='username' placeholder='Username' />
-			</div>
-			<div class='pure-control-group'>
-				<label for='password'>Password</label>
-				<input type='password' name='password' placeholder='Password' />
-			</div>
-			<input type='submit' class='pure-button pure-button-primary' value='Sign in' />
-		</fieldset>
-	</div>
+?><form action="auth.php" method="post">
+	<label>Username:</label>
+	<div class="login_height"><input type="text" name="username" autofocus="autofocus" /></div>
+	<label>Password:</label>
+	<div class="login_height2"><input type="password" name="password" value="" /></div>
+	<div class="input_button"><input type="submit" value="login" /></div>
 </form>
