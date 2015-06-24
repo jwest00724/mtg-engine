9636485a840fc07ca75b19dc994f44211f88998b
Magictallguy <magictallguy@hotmail.com>
2015-06-24 04:29:39 +0100

    Changed to DBAD license, changed layout

--- includes/header.php
+++ includes/header.php
@@ -1,23 +1,21 @@
 <?php
-/*Copyright (c) 2015 Orsokuma
+/*DON'T BE A DICK PUBLIC LICENSE
 
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
+Everyone is permitted to copy and distribute verbatim or modified copies of this license document, and changing it is allowed as long as the name is changed.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+    DON'T BE A DICK PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
+    Do whatever you like with the original work, just don't be a dick.
+
+    Being a dick includes - but is not limited to - the following instances:
+
+    1a. Outright copyright infringement - Don't just copy this and change the name.
+    1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
+    1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
+
+    If you become rich through modifications, related works/services, or supporting the original work, share the love. Only a dick would make loads off this work and not buy the original works creator(s) a pint.
+
+    Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
 */
 if(strpos($_SERVER['REQUEST_URI'], basename(__FILE__)) !== false)
 	exit;
@@ -36,11 +34,7 @@ class headers {
 		$db->execute(array($my['id']));
 		header("Content-type: text/html;charset=UTF-8");
 		?><!doctype html>
-		<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-		<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
-		<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
-		<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
-		<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+		<html lang="en">
 		<head>
 		<base href="http://mtgtest.tk/adz/" />
 		<meta charset="utf-8">
@@ -48,62 +42,41 @@ class headers {
 		<meta name="author" content="Magictallguy" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="css/style.css" />
-		<link rel="stylesheet" href="css/mtg.css" />
-		<!-- superfish style include -->
-		<link rel="stylesheet" href="js/superfish/css/superfish.css" />
-		<script src="js/libs/modernizr-1.6.min.js"></script>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
-		<script>!window.jQuery && document.write(unescape('%3Cscript src="js/libs/jquery-1.4.2.min.js"%3E%3C/script%3E'))</script>
-		<script type="text/javascript" src="js/libs/cufon-yui.js"></script>
-		<script type="text/javascript" src="js/font/Myriad_Pro_400.font.js"></script>
-		<script type="text/javascript" src="js/font/Myriad_Pro_600.font.js"></script>
-		<script type="text/javascript" src="js/font/Myriad_Pro_700.font.js"></script>
-		<script type="text/javascript" src="js/font/Myriad_Pro_300.font.js"></script>
-		<script type="text/javascript" src="js/cufon-replace.js"></script>
+		<link rel="stylesheet" href="css/message.css" />
 		</head>
 		<body>
-		<div id="container">
-		<!-- header -->
-		<header>
-		<!-- .logo -->
-		<div class="logo"> <a href="index.php"><img src="images/logo.png" alt="<?php echo $set['game_name']; ?>" title="<?php echo $set['game_name']; ?>" /></a></div>
-		<!-- /.logo -->
-		<nav>
-			<ul class="sf-menu">
-				<li<?php echo $_SERVER['PHP_SELF'] == '/profile.php' ? " class='current_page_item'" : ''; ?>><?php echo $users->name($my['id']); ?><ul>
-					<li><strong>Energy:</strong> <?php echo $mtg->format($my['energy']); ?>/<?php echo $mtg->format($my['energy_max']); ?></li>
-					<li><strong>Health:</strong> <?php echo $mtg->format($my['health']); ?>/<?php echo $mtg->format($my['health_max']); ?></li>
-					<li><strong>EXP:</strong> <?php echo $mtg->format($my['exp']); ?>/<?php echo $users->expRequired(); ?></li>
-					<li><strong>Money:</strong> <?php echo $set['main_currency_symbol'].$mtg->format($my['money']); ?></li>
-				</ul></li>
-				<li<?php echo $_SERVER['PHP_SELF'] == '/settings.php' ? " class='current_page_item'" : ''; ?>><a href='settings.php' accesskey='2'>Settings</a></li>
-				<li<?php echo $_SERVER['QUERY_STRING'] == 'action=logout' ? " class='current_page_item'" : ''; ?>><a href='?action=logout' accesskey='3'>Logout</a></li>
-			</ul>
-		</nav>
-		</header>
-		 <!-- EOF header -->
-		 <!-- slider -->
-		 <div class="row_top_tile_sub">
-			<div class="row_top_sub">
-				<div class="container">
-					<h1 class="page_title"><?php echo $set['game_name']; ?></h1>
-				</div>
-			</div>
-		 </div>
-		 <!-- EOF slider -->
-		<div id="main" class="shape">
-			<div class="container_12"><?php
-	}
-
-	function menu($my, $mtg, $users, $db) {
+		<div class='logo'>&nbsp;</div>
+		<div class='left-menu'>
+		<div align='center'><?php
 		if(!defined('MENU_ENABLE'))
 			define('MENU_ENABLE', true);
 		if(defined('MENU_STAFF'))
 			require_once(DIRNAME(__DIR__) . '/staff/menu.php');
 		else
 			require_once(__DIR__ . '/menu.php');
-		?><div class="grid_8 pad3">
-			<p><?php
+		?></div>
+		</div>
+		<div class='right-menu'>
+		<strong>Name:</strong> <?php echo $users->name($my['id'], true); ?><br />
+		<strong>Money:</strong> <?php echo $set['main_currency_symbol'].$mtg->format($my['money']); ?><br />
+		<strong>Level:</strong> <?php echo $mtg->format($my['level']); ?><br />
+		<strong>Points:</strong> <?php echo $mtg->format($my['points']); ?><br />
+		<strong>Merits:</strong> <?php echo $mtg->format($my['merits']); ?><br />
+		<div class='title'>STATS</div>
+		<div style='color:white;'>Bars coming soon</div>
+		<span style='color:green;'>ENERGY: <?php echo round($my['energy'] / $my['energy_max'] * 100); ?>%</span><br />
+		<span style='color:red;'>NERVE: <?php echo round($my['nerve'] / $my['nerve_max'] * 100); ?>%</span><br />
+		<span style='color:cyan;'>HAPPY: <?php echo round($my['happy'] / $my['happy_max'] * 100); ?>%</span><br />
+		<span style='color:orange;'>LIFE: <?php echo round($my['health'] / $my['health_max'] * 100); ?>%</span><br />
+		<span style='color:pink;'>EXP: <?php echo $my['exp'].'/'.$users->expRequired(true); ?></span><br />
+		<div class='title'>OTHER</div><br />
+			<div align='center'>
+				<div class='other-button'><a href='staff.php'>STAFF</a></div><br />
+				<div class='other-button'><a href='lists.php?which=members'>MEMBERS</a></div><br />
+				<div class='other-button'><a href='rules.php'>RULES</a></div><br />
+			</div>
+		</div>
+		<div class='content-container'><?php
 		if(array_key_exists('action', $_GET) && $_GET['action'] == 'logout') {
 			session_unset();
 			session_destroy();
@@ -114,39 +87,9 @@ class headers {
 		if($my['jail'])
 			echo "<strong>Officer:</strong> You're currently in jail for ".$mtg->time_format($my['jail'] * 60).".<br />";
 	}
-
 	function __destruct() {
-		global $set;
-		?>			</p>
-				</div>
-			</div>
-		</div>
-		<!-- footer -->
-		<footer>
-			<div class="foot">
-				<div class="footer">
-					<div class="copy">Copyright &copy; 2011 Forceful Theme. All rights reserved.</div>
-					<div class="bottom_menu">
-						<ul>
-							<li><a href="index.php">Home</a></li>
-							<li><a href="contact.php">Contact</a></li>
-							<li><a href="?action=logout">Logout</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<!-- EOF footer -->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
-		<script type="text/javascript" src="js/jquery.validate.pack.js"></script>
-		<!--[if lt IE 9  ]> <script src="js/cycle/jquery.cycle.all.min_ie.js"></script>     <![endif]-->
-		<!--[if  IE 9 | !(IE)]><!-->  <script src="js/cycle/jquery.cycle.all.min.js" type="text/javascript"></script>  <!--<![endif]-->
-		<!-- super fish js include -->
-		<script type="text/javascript" src="js/superfish/js/superfish.js"></script>
-		<script type="text/javascript" src="js/superfish/js/hoverIntent.js"></script>
-		<!-- scripts concatenated and minified via ant build script-->
-		<script src="js/script.js"></script>
-		<!-- end concatenated and minified scripts-->
+		global $my, $mtg, $users, $set;
+		?></div>
 		</body>
 		</html><?php
 	}
