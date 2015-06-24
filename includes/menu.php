87e1efb1b638bdc9a095d221f86442e1e6602af9
Magictallguy <magictallguy@hotmail.com>
2015-06-24 04:29:51 +0100

    Added to menu

--- includes/menu.php
+++ includes/menu.php
@@ -11,34 +11,30 @@ $links = array(
 	'gym' => 'Gym',
 	'markets' => 'Markets'
 );
-?><div class="grid_4">
-	<div class="sidebar">
-		<ul class="sidebar_menu"><?php
-			foreach($links as $url => $disp) {
-				if(preg_match('/\[msg_count\]/', $disp)) {
-					$db->query("SELECT COUNT(id) FROM users_messages WHERE `read` = 0 AND receiver = ?");
-					$db->execute(array($my['id']));
-					$disp = str_replace('[msg_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
-				}
-				if(preg_match('/\[ev_count\]/', $disp)) {
-					$db->query("SELECT COUNT(id) FROM users_events WHERE `read` = 0 AND user = ?");
-					$db->execute(array($my['id']));
-					$disp = str_replace('[ev_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
-				}
-				if(preg_match('/\[hosp_count\]/', $disp)) {
-					$db->query("SELECT COUNT(id) FROM users WHERE `hospital` > ?");
-					$db->execute(array(time()));
-					$disp = str_replace('[hosp_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
-				}
-				if(preg_match('/\[jail_count\]/', $disp)) {
-					$db->query("SELECT COUNT(id) FROM users WHERE `jail` > ?");
-					$db->execute(array(time()));
-					$disp = str_replace('[jail_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
-				}
-				printf("<li%s><a href='%s.php'>%s</a></li>", $_SERVER['PHP_SELF'] == '/'.$url.'.php' ? " class='current'" : '', $url, $disp);
-			}
-			if($users->hasAccess('staff_panel_access'))
-				echo "<li><a href='staff'>Staff Panel</a></li>";
-	?>		</ul>
-	</div>
-</div>
+foreach($links as $url => $disp) {
+	if(preg_match('/\[msg_count\]/', $disp)) {
+		$db->query("SELECT COUNT(id) FROM users_messages WHERE `read` = 0 AND receiver = ?");
+		$db->execute(array($my['id']));
+		$disp = str_replace('[msg_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
+	}
+	if(preg_match('/\[ev_count\]/', $disp)) {
+		$db->query("SELECT COUNT(id) FROM users_events WHERE `read` = 0 AND user = ?");
+		$db->execute(array($my['id']));
+		$disp = str_replace('[ev_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
+	}
+	if(preg_match('/\[hosp_count\]/', $disp)) {
+		$db->query("SELECT COUNT(id) FROM users WHERE `hospital` > ?");
+		$db->execute(array(time()));
+		$disp = str_replace('[hosp_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
+	}
+	if(preg_match('/\[jail_count\]/', $disp)) {
+		$db->query("SELECT COUNT(id) FROM users WHERE `jail` > ?");
+		$db->execute(array(time()));
+		$disp = str_replace('[jail_count]', '['.$mtg->format($db->fetch_single()).']', $disp);
+	}
+	printf("<div class='button'><a href='%s.php'>%s</a></div>\n<div class='spacer'></div>\n", $url, $disp);
+}
+if($users->hasAccess('staff_panel_access'))
+	echo "<div class='button'><a href='staff'>Staff Panel</a></div>\n<div class='spacer'></div>\n";
+?><div class='button'><a href='?action=logout'>Logout</a></div>
+<div class='spacer'></div>
