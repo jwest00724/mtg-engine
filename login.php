b2dd99d979ec9fb963b61f7022a9b6c74e1b6b92
Magictallguy <magictallguy@hotmail.com>
2015-11-07 10:20:27 +0000

    Added backticks on queries, changed to shorthand arrays, minor UI update

--- login.php
+++ login.php
@@ -1,13 +1,34 @@
 <?php
 require_once(__DIR__ . '/includes/globals_out.php');
-if(isset($_SESSION['msg'])) {
-	$mtg->error($_SESSION['msg'], false);
-	unset($_SESSION['msg']);
-}
-?><form action="auth.php" method="post">
-	<label>Username:</label>
-	<div class="login_height"><input type="text" name="username" autofocus="autofocus" /></div>
-	<label>Password:</label>
-	<div class="login_height2"><input type="password" name="password" value="" /></div>
-	<div class="input_button"><input type="submit" value="login" /></div>
-</form>
+?><div class="header">
+	<div class="logo"></div>
+	<h1>Welcome to <?php echo $mtg->format($set['game_name']);?></h1>
+	<h2 class="content-subhead">Please login</h2>
+</div>
+<div class="content"><?php
+	if(isset($_SESSION['msg'])) {
+		$mtg->error($_SESSION['msg'], false);
+		unset($_SESSION['msg']);
+	}
+	?><form action="auth.php" method="post" class="pure-form pure-form-aligned">
+		<fieldset>
+			<div class="pure-control-group">
+				<label>Username</label>
+				<input type="text" name="username" autofocus="autofocus" required />
+			</div>
+			<div class="pure-control-group">
+				<label>Password</label>
+				<input type="password" name="password" required />
+			</div>
+			<div class="pure-controls">
+				<button type="submit" class="pure-button pure-button-primary">Login</button>
+			</div>
+		</fieldset>
+	</form>
+	Not got an account? <a href="signup.php">Sign up for free</a>
+</div>
+<script>
+$.post("signup.php", function(data) {
+	$("#message").html(data);
+});
+</script>
