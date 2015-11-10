91e4d526498b57d10db7ef5a4c1b372291432727
Magictallguy <magictallguy@hotmail.com>
2015-11-10 08:08:16 +0000

    Minor code optimization

--- staff/index.php
+++ staff/index.php
@@ -1,12 +1,12 @@
 <?php
 define('MENU_STAFF', true);
-require_once(DIRNAME(__DIR__) . '/includes/globals.php');
+require_once DIRNAME(__DIR__) . '/includes/globals.php';
 $_SESSION['staff_authed_time'] = time() + 900;
 $_GET['pull'] = isset($_GET['pull']) && ctype_alpha(str_replace('.php', '', $_GET['pull'])) ? str_replace('.php', '', $_GET['pull']) : null;
 $file = empty($_GET['pull']) ? 'index.php' : $_GET['pull'].'.php';
 
 $path = __DIR__ . '/pull/'.$file;
 if(file_exists($path))
-	include($path);
+	require_once $path;
 else
 	$mtg->error("Invalid pull request".($users->hasAccess('override_all') ? ' - '.$path : ''));
