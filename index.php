c03bb285365b59c930abb6139b92bf5d377d3f9d
Magictallguy <magictallguy@hotmail.com>
2015-11-11 10:54:44 +0000

    Minor UI update

--- index.php
+++ index.php
@@ -2,47 +2,47 @@
 define('HEADER_TEXT', 'Home');
 require_once(__DIR__ . '/includes/globals.php');
 ?><p>
-	<span style='font-size:1.3em;'>Hello there <?php echo $users->name($my['id']); ?>!</span><br />
-	<span style='font-size:1.1em;'>Welcome <?php echo $my['last_seen'] ? 'back ' : ''; ?>to <?php echo $set['game_name']; ?></span>
+	<span style='font-size:1.3em;'>Hello there <?php echo $users->name($my['id']);?>!</span><br />
+	<span style='font-size:1.1em;'>Welcome <?php echo $my['last_seen'] ? 'back ' : '';?>to <?php echo $set['game_name'];?></span>
 </p>
 <table width='100%'>
 	<tr>
 		<th width='12.5%'>Name</th>
-		<td width='37.5%'><?php echo $users->name($my['id'], true); ?></td>
-		<th width='12.5%'>Level/EXP</th>
-		<td width='37.5%'><?php echo $mtg->format($my['level']); ?> (<?php echo $mtg->format($my['exp']); ?>/<?php echo $users->expRequired(); ?>)</td>
+		<td width='37.5%' class="right"><?php echo $users->name($my['id']);?></td>
+		<th width='12.5%'>Points</th>
+		<td width='37.5%' class="right"><?php echo $mtg->format($my['points']);?></td>
 	</tr>
 	<tr>
 		<th>Money</th>
-		<td><?php echo $set['main_currency_symbol'].$mtg->format($my['money']); ?></td>
+		<td class="right"><?php echo $set['main_currency_symbol'].$mtg->format($my['money']);?></td>
 		<th>Bank</th>
-		<td><?php echo $my['bank'] > -1 ? $set['main_currency_symbol'].$mtg->format($my['bank']) : 'No account'; ?></td>
+		<td class="right"><?php echo $my['bank'] > -1 ? $set['main_currency_symbol'].$mtg->format($my['bank']) : 'No account';?></td>
 	</tr>
 	<tr>
-		<th>Points</th>
-		<td><?php echo $mtg->format($my['points']); ?></td>
-		<th>&nbsp;</th>
-		<td>&nbsp;</td>
+		<th>Level</th>
+		<td class="right"><?php echo $mtg->format($my['level']);?></td>
+		<th>EXP</th>
+		<td class="right"><?php echo $mtg->format($my['exp'], 2);?>/<?php echo $users->expRequired();?> (<?php echo $users->expPercent();?>)</td>
 	</tr>
 	<tr>
 		<th colspan='4'>Stats</th>
 	</tr>
 	<tr>
 		<th>Strength</th>
-		<td><?php echo $mtg->format($my['strength']);?></td>
+		<td class="right"><?php echo $mtg->format($my['strength']);?></td>
 		<th>Agility</th>
-		<td><?php echo $mtg->format($my['agility']);?></td>
+		<td class="right"><?php echo $mtg->format($my['agility']);?></td>
 	</tr>
 	<tr>
 		<th>Guard</th>
-		<td><?php echo $mtg->format($my['guard']);?></td>
+		<td class="right"><?php echo $mtg->format($my['guard']);?></td>
 		<th>Labour</th>
-		<td><?php echo $mtg->format($my['labour']);?></td>
+		<td class="right"><?php echo $mtg->format($my['labour']);?></td>
 	</tr>
 	<tr>
-		<th>Intelligence Quotient</th>
-		<td><?php echo $mtg->format($my['iq']);?></td>
+		<th>Intelligence</th>
+		<td class="right"><?php echo $mtg->format($my['iq']);?></td>
 		<th>Total Stats</th>
-		<td><?php echo $mtg->format($my['total_stats']);?></td>
+		<td class="right"><?php echo $mtg->format($my['total_stats']);?></td>
 	</tr>
 </table>
