5ffa1d9e533e555471eabec0c78f475d1c28cd88
Magictallguy <magictallguy@hotmail.com>
2015-11-11 12:41:58 +0000

    Updated to latest local version

--- install/sqls/updates.sql
+++ install/sqls/updates.sql
@@ -2,4 +2,4 @@ DROP TABLE IF EXISTS `crimes`;
 DROP TABLE IF EXISTS `crimegroups`;
 DROP TABLE IF EXISTS `settings_game`;
 RENAME TABLE `game_settings` TO `settings_game`;
-UPDATE `settings_game` SET `value` = '9.0.0170' WHERE `name` = 'engine_version';
+UPDATE `settings_game` SET `value` = '9.0.0175' WHERE `name` = 'engine_version';
