88ecdc0370ceb55cd0b436f9faf95fa27889e161
Magictallguy <magictallguy@hotmail.com>
2015-11-11 14:05:46 +0000

    Updated to latest local version

--- install/sqls/updates.sql
+++ install/sqls/updates.sql
@@ -2,4 +2,5 @@ DROP TABLE IF EXISTS `crimes`;
 DROP TABLE IF EXISTS `crimegroups`;
 DROP TABLE IF EXISTS `settings_game`;
 RENAME TABLE `game_settings` TO `settings_game`;
-UPDATE `settings_game` SET `value` = '9.0.0175' WHERE `name` = 'engine_version';
+ALTER TABLE `staff_ranks` CHANGE `staff_panel_staff_ranks_view` `staff_panel_staff_ranks_manage` ENUM('Yes','No') NOT NULL DEFAULT 'No';
+UPDATE `settings_game` SET `value` = '9.0.0182' WHERE `name` = 'engine_version';
