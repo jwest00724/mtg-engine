eb5fa8c24fb8c44def993da10ead04cc87f3bf9c
Magictallguy <magictallguy@hotmail.com>
2015-11-12 20:10:51 +0000

    Added namespace declaration

--- includes/class/class_mtg_logs.php
+++ includes/class/class_mtg_logs.php
@@ -1,4 +1,5 @@
 <?php
+namespace MTG;
 /*DON'T BE A DICK PUBLIC LICENSE
 
 Everyone is permitted to copy and distribute verbatim or modified copies of this license document, and changing it is allowed as long as the name is changed.
