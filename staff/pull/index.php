4a0cc82130d2d8e40b97868ded319b8e084a5502
Magictallguy <magictallguy@hotmail.com>
2015-11-14 11:31:00 +0000

    Major UI update, minor bug fix

--- staff/pull/index.php
+++ staff/pull/index.php
@@ -77,39 +77,63 @@ function gameSettings($db, $my, $mtg, $set) {
 			$_POST[$what] = isset($_POST[$what]) && ctype_digit(str_replace(',', '', $_POST[$what])) ? str_replace(',', '', $_POST[$what]) : 0;
 		$posted = array_merge($strs, $nums);
 		foreach($posted as $what) {
-			$db->query("UPDATE game_settings SET value = ? WHERE name = ?");
-			$db->execute([$_POST[$what], $posted]);
+			$db->query("UPDATE `settings_game` SET `value` = ? WHERE `name` = ?");
+			$db->execute([$_POST[$what], $what]);
+			$set[$what] = $_POST[$what];
 		}
 		$mtg->success("You've updated the game's settings");
 	}
-	?><form action="?action=settings" method="post" class="pure-form pure-form-aligned">
+	?><form action="staff/?action=settings" method="post" class="pure-form pure-form-aligned">
+		<legend>Basic Settings</legend>
 		<div class="pure-control-group">
-			<span class="pure-heading">Basic Settings</span>
-			<fielset class="pure-group">
-				<label for="name">Name</label>
-				<input type="text" name="game_name" value="<?php echo $mtg->format($set['game_name']);?>" />
-				<label for="owner">Game Owner's ID</label>
-				<input type="text" name="game_owner_id" value="<?php echo $mtg->format($set['game_owner_id']);?>" />
-				<label for="description">Description</label>
-				<textarea name="game_description" rows="10" cols="40"><?php echo $mtg->format($set['game_description']);?></textarea>
-			</fielset>
-			<fieldset class="pure-group">
-				<span class="pure-heading">Registration</span>
-				<label for="cash">Start Cash</label>
-				<input type="text" name="register_start_cash" value="<?php echo $mtg->format($set['register_start_cash']);?>" />
-				<label for="promo-code">Promotional Code</label>
-				<input type="text" name="register_promo_code" value="<?php echo $mtg->format($set['register_promo_code']);?>" />
-				<label for="promo-cash">Promo Cash</label>
-				<input type="text" name="register_promo_cash" value="<?php echo $mtg->format($set['register_promo_cash']);?>" />
-			</fieldset>
-			<fieldset class="pure-group">
-				<span class="pure-heading">Game Settings</span>
-				<label for="curreny-symbol">Currency Symbol</label>
-				<input type="text" name="main_currency_symbol" value="<?php echo htmlentities($mtg->format($set['main_currency_symbol']));?>" />
-			</fieldset>
+			<label for="name">Name</label>
+			<input type="text" name="game_name" value="<?php echo $mtg->format($set['game_name']);?>" class="pure-u-1-3" />
+		</div>
+		<div class="pure-control-group">
+			<label for="owner">Game Owner's ID</label>
+			<input type="text" name="game_owner_id" value="<?php echo $mtg->format($set['game_owner_id']);?>" class="pure-u-1-3" />
+		</div>
+		<div class="pure-control-group">
+			<label for="description">Description</label>
+			<textarea name="game_description" class="pure-u-1-3"><?php echo $mtg->format($set['game_description']);?></textarea>
+		</div>
+		<legend>Registration</legend>
+		<div class="pure-control-group">
+			<label for="cash">Start Cash</label>
+			<input type="text" name="register_start_cash" value="<?php echo $mtg->format($set['register_start_cash']);?>" class="pure-u-1-3" />
+		</div>
+		<div class="pure-control-group">
+			<label for="promo-code">Promotional Code</label>
+			<input type="text" name="register_promo_code" value="<?php echo $mtg->format($set['register_promo_code']);?>" class="pure-u-1-3" />
+		</div>
+		<div class="pure-control-group">
+			<label for="promo-cash">Promo Cash</label>
+			<input type="text" name="register_promo_cash" value="<?php echo $mtg->format($set['register_promo_cash']);?>" class="pure-u-1-3" />
+		</div>
+		<legend>Game Settings</legend>
+		<div class="pure-control-group">
+			<label for="currency-symbol">Currency Symbol</label>
+			<input type="text" name="main_currency_symbol" value="<?php echo htmlentities($mtg->format($set['main_currency_symbol']));?>" class="pure-u-1-3" />
+		</div>
+		<legend>Level Gain Settings</legend>
+		<div class="pure-control-group">
+			<label for="max_health_gained">Maximum Health Increase</label>
+			<input type="text" name="max_health_gained" value="<?php echo $mtg->format($set['max_health_gained']);?>" class="pure-u-1-3" />
+		</div>
+		<div class="pure-control-group">
+			<label for="max_energy_gained">Maximum Energy Increase</label>
+			<input type="text" name="max_energy_gained" value="<?php echo $mtg->format($set['max_energy_gained']);?>" class="pure-u-1-3" />
+		</div>
+		<div class="pure-control-group">
+			<label for="max_power_gained">Maximum Power Increase</label>
+			<input type="text" name="max_power_gained" value="<?php echo $mtg->format($set['max_power_gained']);?>" class="pure-u-1-3" />
+		</div>
+		<div class="pure-control-group">
+			<label for="max_nerve_gained">Maximum Nerve Increase</label>
+			<input type="text" name="max_nerve_gained" value="<?php echo $mtg->format($set['max_nerve_gained']);?>" class="pure-u-1-3" />
 		</div>
 		<div class="pure-controls">
-			<button type="submit" name="submit" class="pure-button pure-button-primary">Update Settings</button>
+			<button type="submit" name="submit" class="pure-button pure-button-primary"><i class="fa fa-cog"></i> Update Settings</button>
 			<button type="reset" class="pure-button pure-button-secondary"><i class="fa fa-recycle"></i> Reset</button>
 		</div>
 	</form><?php
