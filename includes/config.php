c995849979e70f5de32af9fecc115c22c720d225
Magictallguy <magictallguy@hotmail.com>
2015-11-14 11:34:58 +0000

    Removed config file

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!defined('MTG_ENABLE'))
-	exit;
-date_default_timezone_set('Europe/London');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mtgv9');
