90ae2bc9469f92caff41a9a620344c72a5e2899b
Magictallguy <magictallguy@hotmail.com>
2015-11-14 11:30:34 +0000

    Added levelling functionality

--- includes/globals.php
+++ includes/globals.php
@@ -38,6 +38,16 @@ require_once __DIR__ . '/class/class_mtg_users.php';
 require_once __DIR__ . '/class/class_mtg_site.php';
 require_once __DIR__ . '/class/class_mtg_logs.php';
 require_once __DIR__ . '/class/class_mtg_items.php';
+if($my['exp'] >= $users->expRequired(false)) {
+	$db->startTrans();
+	$db->query('UPDATE `users` SET `level` = `level` + ? WHERE `id` = ?');
+	$db->execute([$set['level_gained'], $my['id']]);
+	$db->query('UPDATE `users_stats` SET `health_max` = `health_max` + ?, `health` = ?, `energy_max` = `energy_max` + ?, `energy` = ?, `power_max` = `power_max` + ?, `power` = ?, `nerve_max` = `nerve_max` + ?, `nerve` = ? WHERE `id` + ?');
+	$db->execute([$set['max_health_gained'], $my['health_max'] + $set['max_health_gained'], $set['max_energy_gained'], $my['energy_max'] + $set['max_energy_gained'], $set['max_power_gained'], $my['power_max'] + $set['max_power_gained'], $set['max_nerve_gained'], $my['nerve_max'] + $set['max_nerve_gained'], $my['id']]);
+	$users->send_event($my['id'], 'Achievements', 'Congratulations! You\'ve levelled up! You gained an increase of '.$mtg->format($set['max_health_gained']).' to your maximum health, '.$mtg->format($set['max_energy_gained']).' to your maximum energy, '.$mtg->format($set['max_nerve_gained']).' to your maximum nerve, and '.$mtg->format($set['max_power_gained']).' to your maximum power');
+	$db->endTrans();
+}
+require_once __DIR__ . '/cronless_crons.php';
 require_once __DIR__ . '/header.php';
 $h = MTG\headers::getInstance($db, $set, $my, $mtg, $users);
 if(defined('MENU_STAFF') && !$my['staff_rank'])
