7db7ce321da8d49ae22d31246a63124d291f8020
Magictallguy <magictallguy@hotmail.com>
2015-11-14 06:09:03 +0000

    Required pagination class

--- staff/index.php
+++ staff/index.php
@@ -1,6 +1,8 @@
 <?php
 define('MENU_STAFF', true);
 require_once DIRNAME(__DIR__) . '/includes/globals.php';
+require_once DIRNAME(__DIR__) . '/includes/class/class_mtg_paginate.php';
+$pages = new Paginator();
 $_SESSION['staff_authed_time'] = time() + 900;
 $_GET['pull'] = isset($_GET['pull']) && ctype_alpha(str_replace('.php', '', $_GET['pull'])) ? str_replace('.php', '', $_GET['pull']) : null;
 $file = empty($_GET['pull']) ? 'index.php' : $_GET['pull'].'.php';
