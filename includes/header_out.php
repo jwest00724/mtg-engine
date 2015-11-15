902e404ea701a8b52da0de883f18fb45e2c5a886
Magictallguy <magictallguy@hotmail.com>
2015-11-15 09:33:34 +0000

    CLRF handling changes

--- includes/header_out.php
+++ includes/header_out.php
@@ -34,6 +34,7 @@ class headers {
 		?><!DOCTYPE html>
 		<html lang="en">
 			<head>
+				<base href="http://engine.magictallguy.tk/" />
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<meta name="description" content="MTG Codes v9" />
