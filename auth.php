037fc17893bda41153bad70f2676aee69d70cd80
Magictallguy <magictallguy@hotmail.com>
2015-11-15 16:22:58 +0000

    Updated to use new $_SESSION['msg'] handle

--- auth.php
+++ auth.php
@@ -5,22 +5,34 @@ require_once __DIR__ . '/includes/class/class_mtg_users.php';
 $_POST['username'] = isset($_POST['username']) ? trim($_POST['username']) : null;
 $_POST['password'] = isset($_POST['password']) ? trim($_POST['password']) : null;
 if(empty($_POST['username'])) {
-	$_SESSION['msg'] = "You didn't enter your username";
+	$_SESSION['msg'] = [
+		'type' => 'error',
+		'content' => 'You didn\'t enter your username'
+	];
 	exit(header("Location: login.php"));
 }
 if(empty($_POST['password'])) {
-	$_SESSION['msg'] = "You didn't enter your password";
+	$_SESSION['msg'] = [
+		'type' => 'error',
+		'content' => 'You didn\'t enter your password'
+	];
 	exit(header("Location: login.php"));
 }
 $db->query("SELECT `id`, `password` FROM `users` WHERE `username` = ?");
 $db->execute([$_POST['username']]);
 if(!$db->num_rows()) {
-	$_SESSION['msg'] = "An account with that name wasn't found";
+	$_SESSION['msg'] = [
+		'type' => 'error',
+		'content' => 'An account with that name wasn\'t found'
+	];
 	exit(header("Location: login.php"));
 }
 $user = $db->fetch_row(true);
 if($user['password'] != $users->hashPass($_POST['password'])) {
-	$_SESSION['msg'] = "That password is incorrect";
+	$_SESSION['msg'] = [
+		'type' => 'error',
+		'content' => 'That password was incorrect'
+	];
 	exit(header("Location: login.php"));
 }
 $_SESSION['userid'] = $user['id'];
