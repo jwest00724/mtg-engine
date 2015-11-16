0812821f2c07e495cf7b1fd2ece3936dfd1d57b1
Magictallguy <magictallguy@hotmail.com>
2015-11-16 13:06:23 +0000

    Added automatic logout setting

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 define('HEADER_TEXT', 'Settings');
 require_once __DIR__ . '/includes/globals.php';
 if(array_key_exists('submit', $_POST)) {
-	$values = ['username', 'password', 'cpassword', 'email'];
+	$values = ['username', 'password', 'cpassword', 'email', 'timeout'];
 	foreach($values as $what)
 		$_POST[$what] = array_key_exists($what, $_POST) && isset($_POST[$what]) ? trim($_POST[$what]) : null;
 	$updates = [];
@@ -35,11 +35,26 @@ if(array_key_exists('submit', $_POST)) {
 		$db->execute([$_POST['email'], $my['id']]);
 		$updates[] = 'email';
 	}
+	if(!empty($_POST['timeout'])) {
+		if(!in_array($_POST['timeout'], [300, 900, 1800, 3600, 86400, 'never']))
+			$mtg->error('You didn\'t select a valid timeout');
+		$users->changeSetting('logout_threshold', $_POST['timeout']);
+		$updates[] = 'login timeout';
+	}
 	if(count($updates)) {
 		$what = implode(', ', $updates);
 		$mtg->success('You\'ve updated your '.$what);
 	}
 }
+$logouts = [
+	300 => '5 minutes',
+	900 => '15 minutes',
+	1800 => '30 minutes',
+	3600 => 'An hour',
+	86400 => 'A day',
+	'never' => 'Never'
+];
+$logout = $users->getSetting('logout_threshold');
 ?><form action="settings.php" method="post" class="pure-form pure-form-aligned">
 	<h3 class="content-subhead">Account Settings</h3>
 	<div class="pure-control-group">
@@ -48,7 +63,7 @@ if(array_key_exists('submit', $_POST)) {
 	</div>
 	<div class="pure-control-group">
 		<label for="password">Password</label>
-		<input type="password" name="password" placeholder="Leave blank if you don&apos;t want to change it" class="pure-input-1-2" required />
+		<input type="password" name="password" placeholder="Leave blank if you don&apos;t want to change it" class="pure-input-1-2" />
 	</div>
 	<div class="pure-control-group">
 		<label for="confirmation">Confirm Password</label>
@@ -58,6 +73,15 @@ if(array_key_exists('submit', $_POST)) {
 		<label for="email">Email</label>
 		<input type="email" name="email" placeholder="<?php echo $mtg->format($my['email']);?>" class="pure-input-1-2" />
 	</div>
-	<button type="submit" name="submit" value="true" class="pure-button pure-button-primary">Update Settings</button>
-	<button type="reset" class="pure-button pure-button-secondary"><i class="fa fa-recycle"></i> Reset</button>
+	<div class="pure-control-group">
+		<label for="timeout">Automatic log out</label>
+		<select name="timeout"><?php
+		foreach($logouts as $time => $display)
+			printf('<option value="%s"%s>%s</option>', $time, $time == $logout ? ' selected' : null, $display);
+		?></select>
+	</div>
+	<div class="pure-controls">
+		<button type="submit" name="submit" value="true" class="pure-button pure-button-primary">Update Settings</button>
+		<button type="reset" class="pure-button pure-button-secondary"><i class="fa fa-recycle"></i> Reset</button>
+	</div>
 </form>
