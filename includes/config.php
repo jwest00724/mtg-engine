3c1c3b19a4bc63b1610eeba34219f2a61a6d73ff
Magictallguy <magictallguy@hotmail.com>
2015-11-17 02:56:29 +0000

    Config for Heroku

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+if(!defined('MTG_ENABLE'))
+	exit;
+date_default_timezone_set('Europe/London');
+define('DB_HOST', 'eu-cdbr-west-01.cleardb.com');
+define('DB_USER', 'bb091a27e9e45d');
+define('DB_PASS', '54ee48c8');
+define('DB_NAME', 'heroku_cfbeca4085f5b38');
