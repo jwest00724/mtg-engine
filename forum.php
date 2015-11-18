0103a1724bb200de8cf077fcb3b48f970a6c6458
Magictallguy <magictallguy@hotmail.com>
2015-11-18 15:13:48 +0000

    Major bug fix

--- forum.php
+++ forum.php
@@ -9,6 +9,8 @@ $pages = new Paginator();
 require_once __DIR__ . '/includes/class/jbbcode/Parser.php';
 $parser = new jBBCode\Parser();
 $parser->addCodeDefinitionSet(new JBBCode\DefaultCodeDefinitionSet());
+require_once __DIR__ . '/includes/securimage/securimage.php';
+$securimage = new Securimage();
 function formatLatestPost($id) {
 	global $db, $mtg, $parser;
 	$db->query('SELECT `content` FROM `forums_posts` WHERE `parent_topic` = ? ORDER BY `posted` DESC LIMIT 1');
@@ -25,43 +27,43 @@ $_GET['action'] = array_key_exists('action', $_GET) && ctype_alpha($_GET['action
 $_GET['sub'] = array_key_exists('sub', $_GET) && ctype_alpha($_GET['sub']) ? strtolower(trim($_GET['sub'])) : null;
 switch($_GET['action']) {
 	case 'board':
-		boardView($db, $my, $mtg, $users, $board = 0);
+		boardView($db, $my, $mtg, $set, $users, $board = 0);
 		break;
 	case 'topic':
 		switch($_GET['sub']) {
 			default:
-				topicView($db, $my, $mtg, $users, $topic = 0);
+				topicView($db, $my, $mtg, $set, $users, $topic = 0);
 				break;
 			case 'new':
-				topicNew($db, $my, $mtg, $users);
+				topicNew($db, $my, $mtg, $set, $users, $securimage);
 				break;
 		}
 		break;
 	case 'post':
-		respond($db, $my, $mtg, $users);
+		respond($db, $my, $mtg, $set, $users, $securimage);
 		break;
 	case 'admin':
 		switch($_GET['sub']) {
 			case 'pin':
 				if(!$users->hasAccess('forum_topic_pin'))
 					$mtg->error('You don\'t have access');
-				topicPin($db, $my, $mtg, $users, $logs);
+				topicPin($db, $my, $mtg, $set, $users, $logs);
 				break;
 			case 'lock':
 				if(!$users->hasAccess('forum_topic_lock'))
 					$mtg->error('You don\'t have access');
-				topicLock($db, $my, $mtg, $users, $logs);
+				topicLock($db, $my, $mtg, $set, $users, $logs);
 				break;
 			case 'deletetopic':
 				if(!$users->hasAccess('forum_topic_delete'))
 					$mtg->error('You don\'t have access');
-				topicDelete($db, $my, $mtg, $users, $logs);
+				topicDelete($db, $my, $mtg, $set, $users, $logs);
 				break;
 			case 'editpost':
-				postEdit($db, $my, $mtg, $users, $logs);
+				postEdit($db, $my, $mtg, $set, $users, $logs, $securimage);
 				break;
 			case 'deletepost':
-				postDelete($db, $my, $mtg, $users, $logs);
+				postDelete($db, $my, $mtg, $set, $users, $logs);
 				break;
 			default:
 				$mtg->info('No sub action specified', true);
@@ -69,13 +71,13 @@ switch($_GET['action']) {
 		}
 		break;
 	case 'sub':
-		topicSubscribe($db, $my, $mtg, $users);
+		topicSubscribe($db, $my, $mtg, $set, $users);
 		break;
 	default:
-		index($db, $my, $mtg, $users);
+		index($db, $my, $mtg, $set, $users);
 		break;
 }
-function index($db, $my, $mtg, $users) {
+function index($db, $my, $mtg, $set, $users) {
 	$db->query('SELECT `id`, `name`, `description`, `latest_post_id`, `latest_topic_id`, `latest_post_user`, `latest_post_time` FROM `forums` WHERE `publicity` = "all" ORDER BY `name` ASC');
 	$db->execute();
 	?><h3 class="content-subhead">Public Boards</h3>
@@ -147,7 +149,7 @@ function index($db, $my, $mtg, $users) {
 		?></table></p><?php
 	}
 }
-function boardView($db, $my, $mtg, $users, $board = 0) {
+function boardView($db, $my, $mtg, $set, $users, $board = 0) {
 	global $pages;
 	if($board)
 		$_GET['ID'] = $board;
@@ -197,7 +199,7 @@ function boardView($db, $my, $mtg, $users, $board = 0) {
 	?></table>
 	<div class="paginate"><?php echo $pages->display_pages();?></div><?php
 }
-function topicNew($db, $my, $mtg, $users) {
+function topicNew($db, $my, $mtg, $set, $users, $securimage) {
 	if(empty($_GET['ID']))
 		$mtg->error('You didn\'t specify a valid board');
 	$db->query('SELECT `name`, `publicity`, `is_recycle` FROM `forums` WHERE `id` = ?');
@@ -213,6 +215,11 @@ function topicNew($db, $my, $mtg, $users) {
 		$mtg->error('You can\'t create topics in a recycle board');
 	?><h3 class="content-subhead"><a href="forum.php">Index</a> &rarr; <a href="forum.php?action=board&amp;ID=<?php echo $_GET['ID'];?>"><?php echo $mtg->format($row['name']);?></a> &rarr; Adding a new topic</h3><?php
 	if(array_key_exists('submit', $_POST)) {
+		if($set['captcha_forums']) {
+			$_POST['captcha_code'] = array_key_exists('captcha_code', $_POST) && ctype_digit($_POST['captcha_code']) && strlen($_POST['captcha_code']) == 6 ? $_POST['captcha_code'] : null;
+			if($securimage->check($_POST['captcha_code']) == false)
+				$mtg->error('You didn\'t enter a valid code');
+		}
 		$_POST['name'] = array_key_exists('name', $_POST) && is_string($_POST['name']) ? trim($_POST['name']) : null;
 		if(empty($_POST['name']))
 			$mtg->error('You didn\'t enter a valid name');
@@ -229,7 +236,7 @@ function topicNew($db, $my, $mtg, $users) {
 		$db->endTrans();
 		reStatForum($db, $topic);
 		$mtg->success('&ldquo;'.$mtg->format($_POST['name']).'&rdquo; has been created');
-		boardView($db, $my, $mtg, $users, $_GET['ID']);
+		boardView($db, $my, $mtg, $set, $users, $_GET['ID']);
 	} else {
 		?><form action="forum.php?action=topic&amp;sub=new&amp;ID=<?php echo $_GET['ID'];?>" method="post" class="pure-form pure-form-aligned">
 			<div class="pure-control-group">
@@ -243,15 +250,26 @@ function topicNew($db, $my, $mtg, $users) {
 			<div class="pure-control-group">
 				<label for="content">Post</label>
 				<textarea name="content" class="pure-u-1-2"></textarea>
-			</div>
-			<div class="pure-controls">
+			</div><?php
+			if($set['captcha_forums']) {
+				?><div class="pure-control-group">
+					<label for="image">Captcha Image</label>
+					<img id="captcha" src="/includes/securimage/securimage_show.php" alt="CAPTCHA Image" />
+				</div>
+				<div class="pure-control-group">
+					<label for="code">Captcha Code</label>
+					<input type="text" name="captcha_code" size="10" maxlength="6" class="pure-u-1-3" required />
+					<a href="#" onclick="document.getElementById('captcha').src = '/includes/securimage/securimage_show.php?' + Math.random(); return false">[ Different Image ]</a>
+				</div><?php
+			}
+			?><div class="pure-controls">
 				<button type="submit" name="submit" value="true" class="pure-button pure-button-primary"><i class="fa fa-plus"></i> Post new topic</button>
 				<button type="reset" class="pure-button pure-button-secondary"><i class="fa fa-recycle"></i> Reset</button>
 			</div>
 		</form><?php
 	}
 }
-function topicView($db, $my, $mtg, $users, $topic = 0) {
+function topicView($db, $my, $mtg, $set, $users, $topic = 0) {
 	global $pages, $parser;
 	if($topic)
 		$_GET['ID'] = $topic;
@@ -279,7 +297,7 @@ function topicView($db, $my, $mtg, $users, $topic = 0) {
 	$db->query('SELECT * FROM `forums_posts` WHERE `parent_topic` = ? AND `deleted` = 0 ORDER BY `id` ASC '.$pages->limit);
 	$db->execute([$_GET['ID']]);
 	$posts = $db->fetch_row();
-	?><h3 class="content-subhead"><a href="forum.php?action=board&amp;ID=<?php echo $topic['parent_board'];?>"><?php echo $mtg->format($board['name']);?></a> &rarr; <?php echo $mtg->format($topic['name']);?></a></h3>
+	?><h3 class="content-subhead"><a href="forum.php">Index</a> &rarr; <a href="forum.php?action=board&amp;ID=<?php echo $topic['parent_board'];?>"><?php echo $mtg->format($board['name']);?></a> &rarr; <?php echo $mtg->format($topic['name']);?></a></h3>
 	<div class="small">
 		This topic was created by <?php echo $users->name($topic['creator']);?> on <?php echo date('l, jS F \a\t H:i:s', strtotime($topic['creation_time']));?> and contains <?php echo $mtg->format($pages->items_total);?> post<?php echo $mtg->s($pages->items_total);
 		if($topic['locked'])
@@ -337,7 +355,10 @@ function topicView($db, $my, $mtg, $users, $topic = 0) {
 				$parser->parse(nl2br($mtg->format($post['content'])));
 				?><tr><?php
 					if($post['user']) {
-						?><td><a id="<?php echo $post['id'];?>" style="display:none;"><?php echo $users->name($post['user']);?></a></td>
+						?><td>
+							<?php echo $users->name($post['user']);?><br />
+							<a id="<?php echo $post['id'];?>" class="small gray"><?php echo date('H:i:s d/m/Y', strtotime($post['posted']));?></a>
+						</td>
 						<td><?php
 							if($post['user'] == $my['id'] || $users->hasAccess('forum_post_delete')) {
 								?><div class="forum-functions">
@@ -379,8 +400,19 @@ function topicView($db, $my, $mtg, $users, $topic = 0) {
 				<div class="pure-control-group">
 					<label for="post">Response</label>
 					<textarea name="post" class="pure-u-1-2"></textarea>
-				</div>
-				<div class="pure-controls">
+				</div><?php
+				if($set['captcha_forums']) {
+					?><div class="pure-control-group">
+						<label for="image">Captcha Image</label>
+						<img id="captcha" src="/includes/securimage/securimage_show.php" alt="CAPTCHA Image" />
+					</div>
+					<div class="pure-control-group">
+						<label for="code">Captcha Code</label>
+						<input type="text" name="captcha_code" size="10" maxlength="6" class="pure-u-1-3" required />
+						<a href="#" onclick="document.getElementById('captcha').src = '/includes/securimage/securimage_show.php?' + Math.random(); return false">[ Different Image ]</a>
+					</div><?php
+				}
+				?><div class="pure-controls">
 					<button type="submit" name="submit" value="true" class="pure-button pure-button-primary"><i class="fa fa-envelope"></i> Post your reply</button>
 					<button type="reset" class="pure-button pure-button-secondary"><i class="fa fa-recycle"></i> Reset</button>
 				</div>
@@ -388,9 +420,14 @@ function topicView($db, $my, $mtg, $users, $topic = 0) {
 		</p><?php
 	}
 }
-function respond($db, $my, $mtg, $users) {
+function respond($db, $my, $mtg, $set, $users, $securimage) {
 	if(empty($_GET['ID']))
 		$mtg->error('You didn\'t specify a valid topic in which to respond');
+	if($set['captcha_forums']) {
+		$_POST['captcha_code'] = array_key_exists('captcha_code', $_POST) && ctype_digit($_POST['captcha_code']) && strlen($_POST['captcha_code']) == 6 ? $_POST['captcha_code'] : null;
+		if($securimage->check($_POST['captcha_code']) == false)
+			$mtg->error('You didn\'t enter a valid code');
+	}
 	$db->query('SELECT `name`, `parent_board`, `locked` FROM `forums_topics` WHERE `id` = ?');
 	$db->execute([$_GET['ID']]);
 	if(!$db->num_rows())
@@ -429,9 +466,10 @@ function respond($db, $my, $mtg, $users) {
 	$db->execute([$post, $my['id'], date('Y-m-d H:i:s'), $_GET['ID']]);
 	$db->endTrans();
 	$mtg->success('Your response has been posted');
-	topicView($db, $my, $mtg, $users, $_GET['ID']);
+	unset($_POST);
+	topicView($db, $my, $mtg, $set, $users, $_GET['ID']);
 }
-function topicPin($db, $my, $mtg, $users, $logs) {
+function topicPin($db, $my, $mtg, $set, $users, $logs) {
 	if(empty($_GET['ID']))
 		$mtg->error('You didn\'t specify a valid topic');
 	$db->query('SELECT `name`, `pinned` FROM `forums_topics` WHERE `id` = ?');
@@ -448,9 +486,9 @@ function topicPin($db, $my, $mtg, $users, $logs) {
 	$logs->staff(ucfirst($act).'inned topic: '.$mtg->format($row['name']));
 	$db->endTrans();
 	$mtg->success('You\'ve '.$act.'inned topic: '.$mtg->format($row['name']));
-	topicView($db, $my, $mtg, $users, $_GET['ID']);
+	topicView($db, $my, $mtg, $set, $users, $_GET['ID']);
 }
-function topicLock($db, $my, $mtg, $users, $logs) {
+function topicLock($db, $my, $mtg, $set, $users, $logs) {
 	if(empty($_GET['ID']))
 		$mtg->error('You didn\'t specify a valid topic');
 	$db->query('SELECT `name`, `locked` FROM `forums_topics` WHERE `id` = ?');
@@ -467,9 +505,9 @@ function topicLock($db, $my, $mtg, $users, $logs) {
 	$logs->staff(ucfirst($act).'ocked topic: '.$mtg->format($row['name']));
 	$db->endTrans();
 	$mtg->success('You\'ve '.$act.'ocked topic: '.$mtg->format($row['name']));
-	topicView($db, $my, $mtg, $users, $_GET['ID']);
+	topicView($db, $my, $mtg, $set, $users, $_GET['ID']);
 }
-function topicDelete($db, $my, $mtg, $users, $logs, $id = 0) {
+function topicDelete($db, $my, $mtg, $set, $users, $logs, $id = 0) {
 	if($id)
 		$_GET['id'] = $id;
 	if(empty($_GET['ID']))
@@ -500,9 +538,9 @@ function topicDelete($db, $my, $mtg, $users, $logs, $id = 0) {
 	reStatForum($db, $_GET['ID']);
 	$logs->staff($log.' topic: '.$mtg->format($row['name']));
 	$mtg->success('You\'ve '.strtolower($log).' topic: '.$mtg->format($row['name']));
-	boardView($db, $my, $mtg, $users, $row['parent_board']);
+	boardView($db, $my, $mtg, $set, $users, $row['parent_board']);
 }
-function postEdit($db, $my, $mtg, $users, $logs) {
+function postEdit($db, $my, $mtg, $set, $users, $logs, $securimage) {
 	if(empty($_GET['ID']))
 		$mtg->error('You didn\'t specify a valid post');
 	$db->query('SELECT `user`, `content`, `parent_topic` FROM `forums_posts` WHERE `id` = ?');
@@ -527,6 +565,11 @@ function postEdit($db, $my, $mtg, $users, $logs) {
 	if($board['publicity'] == 'upgraded' && time() > strtotime($my['upgraded']))
 		$mtg->error('That post is from an upgraded only board, you don\'t have access');
 	if(array_key_exists('edits', $_POST)) {
+		if($set['captcha_forums']) {
+			$_POST['captcha_code'] = array_key_exists('captcha_code', $_POST) && ctype_digit($_POST['captcha_code']) && strlen($_POST['captcha_code']) == 6 ? $_POST['captcha_code'] : null;
+			if($securimage->check($_POST['captcha_code']) == false)
+				$mtg->error('You didn\'t enter a valid code');
+		}
 		$_POST['content'] = array_key_exists('content', $_POST) && is_string($_POST['content']) ? trim($_POST['content']) : null;
 		if(empty($_POST['content']))
 			$mtg->error('You didn\'t enter a valid text');
@@ -534,7 +577,7 @@ function postEdit($db, $my, $mtg, $users, $logs) {
 		$db->execute([$_POST['content'], $my['id'], date('Y-m-d H:i:s'), $_GET['ID']]);
 		if($post['user'] != $my['id'])
 			$logs->staff('Edited post ID #'.$mtg->format($_GET['ID']).' in topic: '.$topic['name']);
-		topicView($db, $my, $mtg, $users, $post['parent_topic']);
+		topicView($db, $my, $mtg, $set, $users, $post['parent_topic']);
 	} else {
 		?><form action="forum.php?action=admin&amp;sub=editpost&amp;ID=<?php echo $_GET['ID'];?>" method="post" class="pure-form pure-form-aligned">
 			<div class="pure-control-group">
@@ -544,15 +587,26 @@ function postEdit($db, $my, $mtg, $users, $logs) {
 			<div class="pure-control-group">
 				<label for="content">Post</label>
 				<textarea name="content" class="pure-u-1-2"><?php echo $mtg->format($post['content']);?></textarea>
-			</div>
-			<div class="pure-controls">
+			</div><?php
+			if($set['captcha_forums']) {
+				?><div class="pure-control-group">
+					<label for="image">Captcha Image</label>
+					<img id="captcha" src="/includes/securimage/securimage_show.php" alt="CAPTCHA Image" />
+				</div>
+				<div class="pure-control-group">
+					<label for="code">Captcha Code</label>
+					<input type="text" name="captcha_code" size="10" maxlength="6" class="pure-u-1-3" required />
+					<a href="#" onclick="document.getElementById('captcha').src = '/includes/securimage/securimage_show.php?' + Math.random(); return false">[ Different Image ]</a>
+				</div><?php
+			}
+			?><div class="pure-controls">
 				<button type="submit" name="edits" value="true" class="pure-button pure-button-primary"><i class="fa fa-pencil"></i> Submit Edit</button>
 				<button type="reset" class="pure-button pure-button-secondary"><i class="fa fa-recycle"></i> Reset</button>
 			</div>
 		</form><?php
 	}
 }
-function postDelete($db, $my, $mtg, $users, $logs) {
+function postDelete($db, $my, $mtg, $set, $users, $logs) {
 	if(empty($_GET['ID']))
 		$mtg->error('You didn\'t specify a valid post');
 	$db->query('SELECT `user`, `parent_topic` FROM `forums_posts` WHERE `id` = ? AND `deleted` = 0');
@@ -586,9 +640,9 @@ function postDelete($db, $my, $mtg, $users, $logs) {
 	if($post['user'] != $my['id'])
 		$logs->staff('Deleted post #'.$mtg->format($_GET['ID']).' from forum topic: '.$mtg->format($topic['name']));
 	$mtg->success('You\'ve deleted post #'.$mtg->format($_GET['ID']).' from topic: '.$mtg->format($topic['name']));
-	topicView($db, $my, $mtg, $users, $post['parent_topic']);
+	topicView($db, $my, $mtg, $set, $users, $post['parent_topic']);
 }
-function topicSubscribe($db, $my, $mtg, $users) {
+function topicSubscribe($db, $my, $mtg, $set, $users) {
 	if(empty($_GET['ID']))
 		$mtg->error('You didn\'t specify a valid topic');
 	$db->query('SELECT `name`, `parent_board` FROM `forums_topics` WHERE `id` = ?');
@@ -618,7 +672,7 @@ function topicSubscribe($db, $my, $mtg, $users) {
 		$action = '';
 	}
 	$mtg->success('You\'ve '.$action.'subscribed to the topic: '.$mtg->format($topic['name']));
-	topicView($db, $my, $mtg, $users);
+	topicView($db, $my, $mtg, $set, $users);
 }
 function reStatForum($db, $id) {
 	// Get latest post in topic
